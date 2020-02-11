06a5155fbc6ce12d9195458c76891e10e6004634
ahmed.zien <ahmed.zien@gmail.com>
2020-02-11 06:44:47 +0200

    connect android with Firebase database

--- app/src/main/java/com/example/firebasecourse/MainActivity.java
+++ app/src/main/java/com/example/firebasecourse/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.firebasecourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "Firebase Connection Success", Toast.LENGTH_LONG).show();
     }
 }
