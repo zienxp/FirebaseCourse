db557d748c47ade7df2772b19feb79eeda487aec
ahmed.zien <ahmed.zien@gmail.com>
2020-02-12 19:29:22 +0200

    insert Data in to firebase as auto increment

--- app/src/main/java/com/example/firebasecourse/MainActivity.java
+++ app/src/main/java/com/example/firebasecourse/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebasecourse;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,14 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private EditText userName,userPhone,userEmail;
     private Button buttonSave;
     private DatabaseReference reff;
     private Member member;
+    private long maxid =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +31,19 @@ public class MainActivity extends AppCompatActivity {
         buttonSave=findViewById(R.id.button_save_user);
         member = new Member();
         reff = FirebaseDatabase.getInstance().getReference().child("Member");
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists())
+                    maxid = (dataSnapshot.getChildrenCount());
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
                 member.setName(userName.getText().toString().trim());
                 member.setPhone(userPhone.getText().toString().trim());
                 member.setEmail(userEmail.getText().toString().trim());
-                reff.push().setValue(member);
+                reff.child(String.valueOf(maxid+1)).setValue(member);
 
                 Toast.makeText(MainActivity.this, "Data Inset Successful", Toast.LENGTH_LONG).show();
 
