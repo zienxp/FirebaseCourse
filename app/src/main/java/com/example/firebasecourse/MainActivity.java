2f67e74e4ae8fb62ccc2b9c46c47a752b0a44111
ahmed.zien <ahmed.zien@gmail.com>
2020-02-12 07:02:18 +0200

    add data

--- app/src/main/java/com/example/firebasecourse/MainActivity.java
+++ app/src/main/java/com/example/firebasecourse/MainActivity.java
@@ -3,15 +3,43 @@ package com.example.firebasecourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class MainActivity extends AppCompatActivity {
+    private EditText userName,userPhone,userEmail;
+    private Button buttonSave;
+    private DatabaseReference reff;
+    private Member member;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userName = findViewById(R.id.text_name);
+        userPhone=findViewById(R.id.text_phone);
+        userEmail =findViewById(R.id.text_email);
+        buttonSave=findViewById(R.id.button_save_user);
+        member = new Member();
+        reff = FirebaseDatabase.getInstance().getReference().child("Member");
 
-        Toast.makeText(this, "Firebase Connection Success", Toast.LENGTH_LONG).show();
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                member.setName(userName.getText().toString().trim());
+                member.setPhone(userPhone.getText().toString().trim());
+                member.setEmail(userEmail.getText().toString().trim());
+                reff.push().setValue(member);
+
+                Toast.makeText(MainActivity.this, "Data Inset Successful", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/firebasecourse/Member.java
@@ -0,0 +1,35 @@
+package com.example.firebasecourse;
+
+public class Member {
+    private String name;
+    private String phone;
+    private String Email;
+
+    public Member(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
